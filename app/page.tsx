c28a1a56d46e5eaa1987a034b03c7611c5bfd09e
Jesus <jsu@laine.ai>
2025-11-22 19:44:34 +0800

    links

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,25 @@
+import Link from 'next/link';
 
 export default function Page() {
   return (
     <main>
+        <h1>Welcome to my games</h1>
+        <nav>
+          <ul>
+            <li>
+              <Link href="/chess-problem">Chess Problem</Link>
+            </li>
+            <li>
+              <Link href="/snake">Snake</Link>
+            </li>
+            <li>
+              <Link href="/sudoku">Sudoku</Link>
+            </li>
+            <li>
+              <Link href="/tick-tock-toe">Tick Tock Toe</Link>
+            </li>
+          </ul>
+        </nav>
     </main>
   );
 }
